ef016fde95ba8e3e5ef6e5775bcbe859e2f555fd
Darshan Suresh <darshansuresh1804@gmail.com>
2025-09-23 16:24:02 +0530

    Add array input and display functionality

---
+++ array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main() {
+    int arr[5], i;
+    clrscr();
+    
+    printf("Enter 5 elements: ");
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Array elements are: ");
+    for(i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    getch();
+}
