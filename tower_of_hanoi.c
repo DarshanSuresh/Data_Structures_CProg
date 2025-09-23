9016745d80c7fa0f3fd6b8e5cd2251c81d717790
Darshan Suresh <darshansuresh1804@gmail.com>
2025-09-23 16:46:32 +0530

    Implement Tower of Hanoi solution in C

---
+++ tower_of_hanoi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+void hanoi(int n, char source, char aux, char dest) {
+    if(n==1){ 
+        printf("Move disk 1 from %c to %c\n",source,dest); 
+        return; 
+    }
+    hanoi(n-1, source, dest, aux);
+    printf("Move disk %d from %c to %c\n",n,source,dest);
+    hanoi(n-1, aux, source, dest);
+}
+
+void main() {
+    int n;
+    clrscr();
+    printf("Enter number of disks: ");
+    scanf("%d",&n);
+    printf("Tower of Hanoi moves:\n");
+    hanoi(n,'A','B','C');
+    getch();
+}
