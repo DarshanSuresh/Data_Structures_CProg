fa79d95cd63fcd5d80427203864963d5f71ecd87
Darshan Suresh <darshansuresh1804@gmail.com>
2025-09-29 10:19:22 +0530

    Column Major Addressing

---
+++ col_maj.C
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <conio.h>
+
+#define MAX 10
+
+void main() {
+	int mat[MAX][MAX];
+	int r, c;
+	int base = 1000;
+	int size = 4;
+	int i, j, addr;
+
+	clrscr();
+
+	printf("Enter number of rows and columns: ");
+	scanf("%d %d", &r, &c);
+
+	printf("Enter matrix elements:\n");
+	for (i = 0; i < r; i++) {
+		for (j = 0; j < c; j++) {
+			scanf("%d", &mat[i][j]);
+		}
+	}
+
+	printf("\nColumn Major Addressing:\n");
+	for (j = 0; j < c; j++) {
+		for (i = 0; i < r; i++) {
+			addr = base + ((i)+(j * r)) * size;
+			printf("A[%d][%d] = %d at address %d\n", i, j, mat[i][j], addr);
+		}
+	}
+
+	getch();
+}
